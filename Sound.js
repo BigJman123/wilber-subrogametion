c6fd8c6e9442e057c584870c02c4959df46b0ec0
Jordan <bigjman1234567@yahoo.com>
2017-02-28 19:50:25 +0000

    wip: sound effects

---
+++ Sound.js
@@ -0,0 +1,16 @@
+// music = game.add.audio('bgm');
+    // music.play();
+    
+    jump = game.add.audio('jump');
+    
+    shoot = game.add.audio('shoot');
+    
+    hitplayer = game.add.audio('hitplayer');
+    
+    hitcar = game.add.audio('hitcar');
+    
+    collect = game.add.audio('collect');
+    
+    win = game.add.audio('win')
+    
+    lose = game.add.audio('lose');
