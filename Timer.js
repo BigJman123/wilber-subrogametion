29f5083b5fb41c7153c990063462fca3345557a7
Jordan <bigjman1234567@yahoo.com>
2017-03-14 03:45:37 +0000

    added timer to every level and when the timer reaches 0 player dies

--- Timer.js
+++ Timer.js
@@ -11,7 +11,7 @@ Timer.create = function() {
 };
 
 Timer.addText = function() {
-	timer = game.add.text(100, 100, '90');
+	timer = game.add.text(175, 20, '90');
 }
 
 Timer.update = function() {
@@ -20,4 +20,18 @@ Timer.update = function() {
 
 Timer.updateDisplay = function() {
 	timer.text = 90 - counter;
+	
+	if (timer.text == 0 && youlose == false) {
+	    youlose == true;
+	    timer.kill();
+        player.kill();
+        
+        youlose = true;
+        music.stop();
+        lose.play();
+        player.kill();
+        store.set('score', 0);
+        setTimeout(() => game.add.text(425, 10, 'Game Over', { fontSize: '30px', fill: '#000' }), 500);
+        setTimeout(() => location.reload(), 3000);
+    }
 }
