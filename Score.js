4d0c7dfb32a2a09721aaeb37bdc795ac534abaa5
Jordan <bigjman1234567@yahoo.com>
2017-03-16 04:28:19 +0000

    wip: trying to get new time bonus working

--- Score.js
+++ Score.js
@@ -21,25 +21,32 @@ Score.add = function(value) {
     scoreText.text = 'score: ' + score;
 }
 
-Score.finalScore() {
-	
-}
-
-Score.checkBonus = function() {
+Score.finalScore = function() {
 	if (youwin == true && Score.levelEnd == false) {
 		Score.levelEnd = true;
-
-		if (timer.text >= 60) {
-	        setTimeout(() => game.add.text(365, 50, 'Time Bonus! +250 points!', { fontSize: '25px', fill: '#000' }), 100);
-	        score += 250;
-	    }
-	    
-	    if (playerHealth == 100) {
-	        setTimeout(() => game.add.text(335, 80, 'Full Health Bonus! +250 points!', { fontSize: '25px', fill: '#000' }), 100);
-	        score += 250;
-	    }	
-
-	    scoreText.text = 'score: ' + score;
-        store.set('score', score);
+		
+		game.currentTimer.pause();
+		final = Timer.timeAvailable - counter;
+		score += final * 5;
+		scoreText.text = 'score: ', + score;
 	}
 }
+
+// Score.checkBonus = function() {
+// 	if (youwin == true && Score.levelEnd == false) {
+// 		Score.levelEnd = true;
+
+// 		if (timer.text >= 60) {
+// 	        setTimeout(() => game.add.text(365, 50, 'Time Bonus! +250 points!', { fontSize: '25px', fill: '#000' }), 100);
+// 	        score += 250;
+// 	    }
+	    
+// 	    if (playerHealth == 100) {
+// 	        setTimeout(() => game.add.text(335, 80, 'Full Health Bonus! +250 points!', { fontSize: '25px', fill: '#000' }), 100);
+// 	        score += 250;
+// 	    }	
+
+// 	    scoreText.text = 'score: ' + score;
+//         store.set('score', score);
+// 	}
+// }
