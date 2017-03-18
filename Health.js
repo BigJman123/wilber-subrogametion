5dcd9c700aa4feb8c7dadb67627641ff86f835c2
Jordan <bigjman1234567@yahoo.com>
2017-03-18 06:21:06 +0000

    fixed platforms on level 1 and fixed the hearts sprite

--- Health.js
+++ Health.js
@@ -3,13 +3,14 @@ var Health = {};
 Health.level = 3;
 
 Health.assets = function() {
-    game.load.spritesheet('health', 'assets/hearts 608x44.png', 152, 44);
+    game.load.spritesheet('health', 'assets/hearts.png', 152, 44);
 }
 
 Health.create = function() {
-    Health.sprite = game.add.sprite(900, 40, 'health');
-    Health.sprite.anchor.setTo(.5, .5);
-    Health.sprite.scale.x = -1;
+    Health.sprite = game.add.sprite(860, 10, 'health');
+    Health.sprite.scale.setTo(.8, .8);
+    // Health.sprite.anchor.setTo(.5, .5);
+    // Health.sprite.scale.x = -1;
     Health.set(Health.level);
 }
 
