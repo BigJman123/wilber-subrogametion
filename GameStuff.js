5810a0a17213b05f26ed20f156abfcac66fd7dd0
BigJman123 <BigJman1234567@yahoo.com>
2017-03-30 14:19:51 -0500

    working on firebase stuff

--- GameStuff.js
+++ GameStuff.js
@@ -11,7 +11,7 @@ GameStuff.levelUpdate = function(x, y) {
     }
 }
 
-GameStuff.bossUpdate = function() {
+GameStuff.bossUpdate = function(x, y) {
 	// if joe is dead... end the game
 	// if ken is dead... end the game
 	if(HealthPretend.isEmpty() && youwin == false) {
@@ -20,5 +20,8 @@ GameStuff.bossUpdate = function() {
 		SoundFX.win();
 		player2.kill();
 		bullets2.destroy();
+
+		setTimeout(() => game.add.text(x, y, loadingLevel, { fontSize: '25px', fill: '#ffd799'}), 5000);
+        setTimeout(() => {window.location = nextLevel}, 6000);
 	}
 }
